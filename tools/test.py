5871f5e5a5449d8089737f7c6e7c4d7ee0bfd3b6
qilei123 <qileimail123@gmail.com>
2019-10-25 13:03:19 -0400

    all

--- tools/test.py
+++ tools/test.py
@@ -25,7 +25,7 @@ def single_gpu_test(model, data_loader, show=False):
         with torch.no_grad():
             result = model(return_loss=False, rescale=not show, **data)
         results.append(result)
-        print(data["img_meta"])
+        print(data["img_meta"]["DataContainer"])
         if show:
             model.module.show_result(data, result,dataset=["line","ridge1","ridge2"],out_file="")
 
