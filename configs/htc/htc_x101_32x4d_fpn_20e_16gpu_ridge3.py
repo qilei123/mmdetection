d51c10a8e92dd1ec4305aa5680a31c1f6263478e
qilei123 <qileimail123@gmail.com>
2019-10-25 03:34:47 -0400

    all

--- configs/htc/htc_x101_32x4d_fpn_20e_16gpu_ridge3.py
+++ configs/htc/htc_x101_32x4d_fpn_20e_16gpu_ridge3.py
@@ -273,7 +273,7 @@ log_config = dict(
 total_epochs = 50
 dist_params = dict(backend='nccl')
 log_level = 'INFO'
-work_dir = './work_dirs/htc_x101_32x4d_fpn_20e_ridge3'
+work_dir = './work_dirs/htc_x101_32x4d_fpn_20e_ridge3_2'
 load_from = None
 resume_from = None
 workflow = [('train', 1)]
