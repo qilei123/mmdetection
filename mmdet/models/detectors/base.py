ade0e3fd5833a47749f97ccf768abee4b3d569f6
qilei123 <qileimail123@gmail.com>
2019-11-22 15:32:21 -0500

    all

--- mmdet/models/detectors/base.py
+++ mmdet/models/detectors/base.py
@@ -150,6 +150,7 @@ class BaseDetector(nn.Module):
             ]
             
             labels = np.concatenate(labels)
+            labels = labels[inds, :]
             #print(out_file)
             if out_file=="":
                 mmcv.imshow_det_bboxes(
