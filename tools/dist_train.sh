9b8aeddcec8fd4bab02deb1bbe68e79324feb463
qilei123 <qileimail123@gmail.com>
2019-12-04 18:14:30 -0500

    all

--- tools/dist_train.sh
+++ tools/dist_train.sh
@@ -3,7 +3,7 @@
 PYTHON=${PYTHON:-"python"}
 
 CONFIG=$1
-GPUS=$2
+GPUS=$1
 
 $PYTHON -m torch.distributed.launch --nproc_per_node=$GPUS \
     $(dirname "$0")/train.py $CONFIG --launcher pytorch ${@:3}
